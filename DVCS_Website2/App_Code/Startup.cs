6e629dba7d7a6879f004fd5677f1d5b2897426a3
nbrownlee2 <nbrownlee2@studentmail.nait.ca>
2015-09-21 13:51:30 -0600

    Creation of DVCS_Website2

---
+++ DVCS_Website2/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DVCS_Website2.Startup))]
+namespace DVCS_Website2
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
